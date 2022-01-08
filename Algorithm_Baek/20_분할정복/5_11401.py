b3b48ddff2b138a7b0e3fe151db8c4c0c527645b
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-01-08 17:26:32 +0900

    이분탐색

--- Algorithm_Baek/20_분할정복/5_11401.py
+++ Algorithm_Baek/20_분할정복/5_11401.py
@@ -3,7 +3,13 @@ input = sys.stdin.readline
 
 a,b = map(int,input().split())
 
-def f(a,b):
+memo = [-1 for _ in range(4_000_000_0)]
+def f(a):
+    if a == 0:
+        return 1
+    if a == 1:
+        return 1
+    pass
     # exit point2
 
     
