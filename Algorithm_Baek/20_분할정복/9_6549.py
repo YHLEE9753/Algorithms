fbbecae86203672f3197bec46758bc21b17b806c
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-01-08 16:56:43 +0900

    분할_6549,2261

--- Algorithm_Baek/20_분할정복/9_6549.py
+++ Algorithm_Baek/20_분할정복/9_6549.py
@@ -1,43 +1,25 @@
-import sys
-input = sys.stdin.readline
-
-# 값 받기
-while(True):
-    val = input()
-    if val == "0\n":
+while True:
+    n, *heights = list(map(int, input().split()))
+    if(n==0):
         break
-    else:
-        result = []
-        heights = list(map(int,val.split()))[:1]
-        heights.insert(0,0)
-        heights.append(0)
-        # heights = [[i,v] for i,v in enumerate(heights)]
 
-        stack = []
-        for idx, height in enumerate(heights):
-            # idx = height[0]
-            # h = height[1]
+    heights.insert(0,0)
+    heights.append(0)
 
-            if (not stack) or stack[-1][1]<=height[1]:
-                stack.append(height)
-            else:
-                while stack[-1][1] > height[1]:
-                    pop_height = stack.pop()
-                    result.append((idx-pop_height[0])*(pop_height[1]))
-                    if not stack:
-                        break             
-                stack.append(height)
-        # 스택 비우기
-        print(stack)
-        final_idx = len(heights)
-        while stack:
-            pop_height = stack.pop()
-            print(pop_height)
-            result.append((final_idx-pop_height[0])*(pop_height[1]))
-        print(result)
-        print(max(result))
-                
-                
+    checked = [0]
+    area = 0
 
+    # 현재 높이보다 이전 높이가 높으면, while 진입
+    # 현재 높이가 더 낮으면 넓이를 이어서 계산할 수 없다.
+    # 따라서 이전 시점까지 저장되었던 사각현의 높이를 계산한다
+    # 끝 사각형도 고려해야 하므로, n+1 까지 반복한다.
+    for i in range(1, n+2): # 0 과 n+1 은 비교군
+        # heights[checked[-1]]은 이전 시점의 사각형 높이
+        # heights[i] 는 현재 시점의 사각형 높이
+        # heights[checked[-1]] > heights[i] 는 현재 높이보다 이전 높이가 높은지 확인
+        while(checked and (heights[checked[-1]]> heights[i])):
 
-
+            cur_height = checked.pop()
+            area = max(area, (i-1-checked[-1]) * heights[cur_height])
+        checked.append(i)
+    print(area)
--- Algorithm_Baek/20_분할정복/test.py
+++ Algorithm_Baek/20_분할정복/test.py
@@ -1,2 +1,25 @@
-stack = []
-print(stack[-1])
+while True:
+    n, *heights = list(map(int, input().split()))
+    if(n==0):
+        break
+
+    heights.insert(0,0)
+    heights.append(0)
+
+    checked = [0]
+    area = 0
+
+    # 현재 높이보다 이전 높이가 높으면, while 진입
+    # 현재 높이가 더 낮으면 넓이를 이어서 계산할 수 없다.
+    # 따라서 이전 시점까지 저장되었던 사각현의 높이를 계산한다
+    # 끝 사각형도 고려해야 하므로, n+1 까지 반복한다.
+    for i in range(1, n+2): # 0 과 n+1 은 비교군
+        # heights[checked[-1]]은 이전 시점의 사각형 높이
+        # heights[i] 는 현재 시점의 사각형 높이
+        # heights[checked[-1]] > heights[i] 는 현재 높이보다 이전 높이가 높은지 확인
+        while(checked and (heights[checked[-1]]> heights[i])):
+
+            cur_height = checked.pop()
+            area = max(area, (i-1-checked[-1]) * heights[cur_height])
+        checked.append(i)
+    print(area)
