2a5a66afd333aa12398a652c4c3a659d52f4975b
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-01-15 18:29:27 +0900

    이분탐색 완

--- Algorithm_Baek/21_이분탐색/7_12015.py
+++ Algorithm_Baek/21_이분탐색/7_12015.py
@@ -2,6 +2,22 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-A = list(map(int,input().split()))
+cases = list(map(int,input().split()))
+memo = [0]
 
+for case in cases:
+    if memo[-1] < case:
+        memo.append(case)
+    else:
+        start = 0
+        end = len(memo)
 
+        while start < end:
+            mid = (start + end) // 2
+            if memo[mid] < case:
+                start = mid + 1
+            else:
+                end = mid
+        memo[end] = case
+
+print(len(memo)-1)
--- Algorithm_Baek/21_이분탐색/test.py
+++ Algorithm_Baek/21_이분탐색/test.py
@@ -1,26 +1,4 @@
-import sys
-input = sys.stdin.readline
-
-k, n = map(int, input().split())
-lines = [int(input()) for _ in range(k)]
-lines.sort()
-start, end = [1, max(lines)]
-
-def find(data, val):
-    result = 0
-    for d in data:
-        result += d // val
-    return result
-
-while start <= end:
-    mid = (start + end) // 2
-    value = find(lines, mid)
-
-    if value < n:
-        end = mid - 1
-    else:
-        start = mid + 1
-
-print(end)
-
-
+x = [1,2,3,4,5]
+for i,v in enumerate(x):
+    print(i)
+    print(v)
