1293286e9c76266a3c3adfec061f527fb0e1c7de
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-01-15 13:59:56 +0900

    이분탐색

--- Algorithm_Baek/21_이분탐색/5_2119.py
+++ Algorithm_Baek/21_이분탐색/5_2119.py
@@ -4,26 +4,19 @@ input = sys.stdin.readline
 n, c = map(int, input().split())
 houses = [int(input()) for _ in range(n)]
 houses.sort()
-start, end = [1, houses[-1]]
+start, end = [1, houses[-1]] 
 
 while start <= end:
     mid = (start + end) // 2
-    print(mid)
+    point = houses[0] # 맨 처음 시작점은 맨 앞의 점
+    count = 1 # 공유기 갯수와 비교하기 위한 변수
 
-    point = houses[0]
-    count = 1
-    flag = True
-    while count < c:
-        print("!")
-        point += mid
-        flag2 = True
-        for h in houses:
-            if h >= point:
-                count += 1
-                point = h
-                break
-        
-    if flag:
+    for h in houses: # 모든 집들을 검토
+        if h >= point + mid: # point 에서 mid 를 더한다.
+            count += 1 # 그다음 점 존재하므로 count 1증가
+            point = h # 포인트를 그다음 점으로 옮긴다. 그러면 자동으로 if 절에서 그 다음 값을 찾을 것이다.
+            
+    if count >= c: 
         start = mid + 1
     else:
         end = mid - 1
