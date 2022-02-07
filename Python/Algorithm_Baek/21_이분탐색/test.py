48fa6f069b5bb15f9b296714edd931fcbc925bfb
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-02-08 01:06:09 +0900

    change directory

--- Python/Algorithm_Baek/21_이분탐색/test.py
+++
@@ -1,4 +0,0 @@
-x = [1,2,3,4,5]
-for i,v in enumerate(x):
-    print(i)
-    print(v)
--- Python/Algorithm_Baek/24_DFS,BFS/test.py
+++
@@ -1,5 +0,0 @@
-graph1 = [[0 for _ in range(5)] for _ in range(5)]
-graph2 = [[0]*5 for _ in range(5)]
-
-print(graph1)
-print(graph2)
