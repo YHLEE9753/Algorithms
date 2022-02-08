d96be1237e2e01026807e923d521a4a1e3e1dcf4
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-02-08 15:47:12 +0900

    11 브루트포스

--- bruteForce_11/A5_MovieDriector.java
+++ bruteForce_11/A5_MovieDriector.java
@@ -1,7 +1,24 @@
 package bruteForce_11;
 
+import java.util.Scanner;
+
 public class A5_MovieDriector {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
 
+        int count = 0;
+        String check = "666";
+        for(int i = 1;i<10000;i++){
+            String str = String.valueOf(i);
+            if(str.contains(check)){
+                count++;
+            }
+            if(count == n){
+                System.out.println(i);
+                break;
+            }
+        }
     }
 }
