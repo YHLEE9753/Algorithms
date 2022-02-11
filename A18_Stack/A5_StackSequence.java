7f5e3bc0c5472d01c88753c83d1f381cc0a73e84
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-02-11 17:03:43 +0900

    change directory

--- A18_Stack/A5_StackSequence.java
+++ A18_Stack/A5_StackSequence.java
@@ -1,36 +1,47 @@
 package A18_Stack;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class A5_StackSequence {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] numbers = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            numbers[i] = scanner.nextInt();
+        }
         Stack<Integer> stack = new Stack<>();
-        int index = 1;
-        for (int i = 0; i < n; i++) {
-            int num = sc.nextInt();
-            while(true){
-                if(num>index){
-                    System.out.println("+");
-                    stack.push(index);
-                    index++;
-                }else if(num<index){
-                    System.out.println("-");
-                    stack.pop();
-                    index--;
-                }else{
-                    System.out.println("+");
-                    stack.push(index);
-                    System.out.println("-");
-                    index--;
-                    stack.pop();
-                    break;
+        List<Character> results = new ArrayList<>();
+
+        int count = 1;
+        boolean isAble = true;
+
+        for (int i = 0; i < N; i++) {
+            int n = numbers[i];
+
+            if (isAble) {
+                while (count <= n) {
+                    stack.add(count++);
+                    results.add('+');
+                }
+
+                if (stack.empty()) {
+                    isAble = false;
+                } else {
+                    while (!stack.empty() && stack.peek() >= n) {
+                        stack.pop();
+                        results.add('-');
+                    }
                 }
             }
-
-
         }
     }
 }
